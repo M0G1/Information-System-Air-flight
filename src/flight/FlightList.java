f74658f8e23ac9c74074dbe337b07daf9e493509
M0G1 <44916652+M0G1@users.noreply.github.com>
2019-11-15 17:32:15 +0400

    Не тестил, но скомпилировал. Добавил кучу классов и заполнил их.

--- src/flight/FlightList.java
+++ src/flight/FlightList.java
@@ -3,34 +3,41 @@ package flight;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
 /**
  * Для работы использовать FlightController.
  * Класс будет хранить все существующие рейсы,
  * осуществлять методы доступа,
- * сохраянять и загружать список рейсов.*/
-public class FlightList implements Iterable<Flight>{
+ * сохраянять и загружать список рейсов.
+ */
+public class FlightList implements Iterable<Flight> {
     private List<Flight> flightList;
-    public  FlightList(){
+
+    {
         flightList = new ArrayList<>();
     }
-    public Flight get(int index){
+
+    private FlightList() {
+    }
+
+    public Flight get(int index) {
         return flightList.get(index);
     }
 
 
-    public boolean add(Flight flight){
+    public boolean add(Flight flight) {
         return flightList.add(flight);
     }
 
-    public boolean remove(Flight flight){
+    public boolean remove(Flight flight) {
         return flightList.remove(flight);
     }
 
-    public Flight remove(int index){
+    public Flight remove(int index) {
         return flightList.remove(index);
     }
 
-    public int size(){
+    public int size() {
         return flightList.size();
     }
 
